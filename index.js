9b544823d04903f0207109265e76a378a3d386be
Kingsly62 <clinton.makori@student.moringaschool.com>
2021-11-13 00:32:17 +0300

    Add a new array for maleAkans and femaleAkans

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Friday","Sarturday"];
+
+let maleAkans = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
