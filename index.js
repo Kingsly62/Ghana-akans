ce380e2b6179c95cbbab97f571ca149b5cbf5134
Kingsly62 <clinton.makori@student.moringaschool.com>
2021-11-13 00:54:00 +0300

    add function akanFamily()

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Friday","Sarturday"];
+let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Friday","Sarturday"]
 
-let maleAkans = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-let femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+let maleAkans = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+function akanFamily(){
+let date = document.getElementById("date").value;
+let gender = document.getElementById("gender").value;
+let dayNumber = new Date(date).getDay()
+
+}
